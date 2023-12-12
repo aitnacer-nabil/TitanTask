cda8e5fd95b1608bc0088972288de86b2d804f22
MounaGuelsa <mounaguelsa9991@gmail.com>
2023-12-12 11:16:53 +0100

    add methods

--- src/main/java/org/titans/services/TaskDAO.java
+++ src/main/java/org/titans/services/TaskDAO.java
@@ -1,5 +1,24 @@
 package org.titans.services;
 
+import org.titans.entities.Category;
+import org.titans.entities.Priority;
+import org.titans.entities.Task;
+
+import java.util.List;
+
 public interface TaskDAO {
+    Task addTask();
+    Task updateTask(Task t,Integer id);
+    void deleteTask(Integer id );
+    List<Task> getAllTasks();
+    List<Task> sortByDate();
+
+    List<Task> sortByPriority();
+    List<Task> sortByCategory();
+    void addPriorityToTask(Priority priority);
+    void addCategoryToTask(Category category);
+
+    List <Task> filterByCategory(Category category);
+
 
 }
--- src/main/java/org/titans/services/TaskDAOImp.java
+++ src/main/java/org/titans/services/TaskDAOImp.java
@@ -1,4 +1,59 @@
 package org.titans.services;
 
+import org.titans.entities.Category;
+import org.titans.entities.Priority;
+import org.titans.entities.Task;
+
+import java.util.List;
+
 public class TaskDAOImp implements TaskDAO {
+    @Override
+    public Task addTask() {
+        return null;
+    }
+
+    @Override
+    public Task updateTask(Task t, Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteTask(Integer id) {
+
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return null;
+    }
+
+    @Override
+    public List<Task> sortByDate() {
+        return null;
+    }
+
+    @Override
+    public List<Task> sortByPriority() {
+        return null;
+    }
+
+    @Override
+    public List<Task> sortByCategory() {
+        return null;
+    }
+
+    @Override
+    public void addPriorityToTask(Priority priority) {
+
+    }
+
+    @Override
+    public void addCategoryToTask(Category category) {
+
+    }
+
+    @Override
+    public List<Task> filterByCategory(Category category) {
+        return null;
+    }
 }
