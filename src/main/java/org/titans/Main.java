4678429d3e173a842a895d8820d84267512a59b8
MounaGuelsa <mounaguelsa9991@gmail.com>
2023-12-12 14:52:47 +0100

    sortBy priority

--- src/main/java/org/titans/Main.java
+++ src/main/java/org/titans/Main.java
@@ -7,14 +7,22 @@ import org.titans.services.TaskDAOImp;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         TaskDAOImp taskDAOImp = new TaskDAOImp();
-        Task task = new Task("2","HEllo",
-                "World", Timestamp.valueOf(LocalDateTime.now())
-                ,new Category("dev","development"),
-                Priority.HAUTE);
-        System.out.println(taskDAOImp.updateTask(task,2));
+        List<Task> t = new ArrayList<>();
+        t=taskDAOImp.sortByPriority();
+        for (Task task : t
+             ) {
+            System.out.println(task);
+        }
+//        Task task = new Task("2","HEllo",
+//                "World", Timestamp.valueOf(LocalDateTime.now())
+//                ,new Category("dev","development"),
+//                Priority.HAUTE);
+//        System.out.println(taskDAOImp.updateTask(task,2));
     }
 }
